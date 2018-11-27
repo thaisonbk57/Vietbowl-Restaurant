2bc6d0f9819a643487b79ad0c198c256843dfe7a
Thaison Dang <thaisondang.bk57@gmail.com>
2018-11-27 20:48:31 +0100

    update the RESERVE button based on the Submitting Status of the FORM

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,8 @@
-$(document).ready(function() {
+let submitting = false; // sending reservation to server and waiting for response
+
+/////////////////////////////////////////////
+
+function heroSectionHeightJustify() {
   // get the height of the header section
   let headerHeight = $(".header").outerHeight();
   // set the padding-top of MAIN section based on the height of header section
@@ -10,34 +14,70 @@ $(document).ready(function() {
   $(".hero").css({
     height: `calc(100vh - ${headerHeight}px)`
   });
+}
 
+function submitReservation() {
+  // change submitting to TRUE and update the RESERVE button
+  submitting = true;
+  updateReserveButton(submitting);
+
+  const [name, phone, date, time, seat, email, note] = [
+    $("[name='name']").val(),
+    $("[name='phone']").val(),
+    $("[name='date']").val(),
+    $("[name='time']").val(),
+    $("[name='seat']").val(),
+    $("[name='email']").val(),
+    $("[name='note']").val()
+  ];
+
+  const data = { name, phone, date, time, seat, email, note };
+
+  $.post(
+    "https://vietbowl-mail.herokuapp.com/new-reservation",
+    data,
+    (res, status) => {
+      if (status === "success") {
+        $("#server-msg").text(res.message);
+        $("#server-msg").addClass(res.status); // 'OK' ||  'ERROR'
+      }
+
+      // change the submitting to FALSE and update the RESERVE button
+      submitting = false;
+      updateReserveButton(submitting);
+
+      // Reset the form
+      $("#reservation-form").trigger("reset");
+    }
+  );
+}
+
+/*
+ * update the RESERVE button including the text and disable/ enable the button
+ * depends on the FORM Submitting status
+ *
+ * @param: status: Boolean.
+ *
+ */
+function updateReserveButton(status) {
+  // if the form is sending data to server
+  if (status) {
+    $("#reservation-submit-btn").val("SENDING...");
+    $("#reservation-submit-btn").attr("disabled", "true");
+  } else {
+    $("#reservation-submit-btn").val("RESERVE");
+    $("#reservation-submit-btn").removeAttr("disabled");
+  }
+}
+
+////////////////////////////////////////////
+
+$(document).ready(function() {
+  heroSectionHeightJustify();
+
+  // reserve a table
   $("#reservation-submit-btn").on("click", e => {
     e.preventDefault();
     submitReservation();
   });
-
-  function submitReservation() {
-    const [name, phone, date, time, seat, email, note] = [
-      $("[name='name']").val(),
-      $("[name='phone']").val(),
-      $("[name='date']").val(),
-      $("[name='time']").val(),
-      $("[name='seat']").val(),
-      $("[name='email']").val(),
-      $("[name='note']").val()
-    ];
-
-    const data = { name, phone, date, time, seat, email, note };
-
-    $.post(
-      "https://vietbowl-mail.herokuapp.com/new-reservation",
-      data,
-      function(res, status) {
-        if (status) {
-          $("#server-msg").text(res.message);
-          $("#server-msg").addClass(res.status);
-        }
-      }
-    );
-  }
 });
